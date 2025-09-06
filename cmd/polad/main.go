cf30506a6bd50f984729508f7e9eb7232a9185a1
k1yoto <nakatakiyoto2022@gmail.com>
2025-09-06 08:45:03 +0000

    feat(main): integrate H-PCE modes with unified server architecture

--- cmd/polad/main.go
+++ cmd/polad/main.go
@@ -71,51 +71,44 @@ func main() {
 		}
 	}()
 
-	switch c.Global.Mode {
-	case "pce":
-		// Prepare TED update tools
-		var tedElemsChan chan []table.TEDElem
-		if c.Global.TED.Enable {
-			switch c.Global.TED.Source {
-			case "gobgp":
-				tedElemsChan = startGoBGPUpdate(&c, logger)
-				if tedElemsChan == nil {
-					logger.Panic("GoBGP update channel is nil")
-					log.Panic("GoBGP update channel is nil")
-				}
-			default:
-				logger.Panic("Specified TED source is not defined")
-				log.Panic("specified TED source is not defined")
-			}
-		}
+	// Prepare TED update tools for TED-enabled modes
+	var tedElemsChan chan []table.TEDElem
+	mode := server.PCEMode(c.Global.Mode)
 
-		// Start PCE server
-		o := &server.PCEOptions{
-			PCEPAddr:  c.Global.PCEP.Address,
-			PCEPPort:  c.Global.PCEP.Port,
-			GRPCAddr:  c.Global.GRPCServer.Address,
-			GRPCPort:  c.Global.GRPCServer.Port,
-			TEDEnable: c.Global.TED.Enable,
-			USidMode:  c.Global.USidMode,
+	// Create unified options
+	o := &server.CommonOptions{
+		Mode:           mode,
+		PCEPServerAddr: c.Global.PCEPServer.Address,
+		PCEPServerPort: c.Global.PCEPServer.Port,
+		PCEPClientAddr: c.Global.PCEPClient.Address,
+		PCEPClientPort: c.Global.PCEPClient.Port,
+		GRPCAddr:       c.Global.GRPCServer.Address,
+		GRPCPort:       c.Global.GRPCServer.Port,
+		USidMode:       c.Global.USidMode,
+		DomainID:       c.Global.DomainID,
+	}
+
+	if c.Global.TED != nil && c.Global.TED.Enable && (mode == server.ModePCE || mode == server.ModeParentPCE || mode == server.ModeChildPCE) {
+		o.TEDEnable = c.Global.TED.Enable
+		switch c.Global.TED.Source {
+		case "gobgp":
+			tedElemsChan = startGoBGPUpdate(&c, logger)
+			if tedElemsChan == nil {
+				logger.Panic("GoBGP update channel is nil")
+				log.Panic("GoBGP update channel is nil")
+			}
+		default:
+			logger.Panic("Specified TED source is not defined")
+			log.Panic("specified TED source is not defined")
 		}
-		if serverErr := server.NewPCE(o, logger, tedElemsChan); serverErr.Error != nil {
-			logger.Panic("Failed to start new server", zap.String("server", serverErr.Server), zap.Error(serverErr.Error))
-			log.Panicf("failed to start new server: %v", serverErr.Error)
-		}
-	case "pcc":
-		// Start PCC client
-		fmt.Printf("PCE Address: %s, PCC Adress: %s\n", c.Global.PCC.PCEAddr, c.Global.PCC.PCCAddr)
-		o := &server.PCCOptions{
-			PCEAddr:  c.Global.PCC.PCEAddr,
-			PCCAddr:  c.Global.PCC.PCCAddr,
-			PCEPPort: c.Global.PCC.Port,
-			GRPCAddr: c.Global.GRPCServer.Address,
-			GRPCPort: c.Global.GRPCServer.Port,
-			USidMode: c.Global.USidMode,
-		}
-		if serverErr := server.NewPCC(o, logger); serverErr.Error != nil {
-			logger.Panic("Failed to start new PCC server", zap.String("server", serverErr.Server), zap.Error(serverErr.Error))
-			log.Panicf("failed to start new PCC server: %v", serverErr.Error)
+	}
+
+	// Validate mode
+	switch mode {
+	case server.ModePCC, server.ModePCE, server.ModeChildPCE, server.ModeParentPCE:
+		// Start unified PCE server
+		if serverErr := server.NewPCEServer(o, logger, tedElemsChan); serverErr.Error != nil {
+			log.Panicf("failed to start PCE server (mode: %s): %v", mode, serverErr.Error)
 		}
 	default:
 		logger.Panic("Invalid mode specified", zap.String("mode", c.Global.Mode))
